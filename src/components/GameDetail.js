5dbc4931412812cbf0d53c0958eb145eb1aa2f8d
NeverLost-Never <haileyesus.mieron@gmail.com>
2022-01-28 02:24:59 +0400

    14. Awesome Redux Trick

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -7,42 +7,46 @@ import { useSelector } from "react-redux";
 
 const GameDetail = () => {
 	//Data
-	const { game, screen } = useSelector((state) => state.detail);
+	const { game, screen, isLoading } = useSelector((state) => state.detail);
 
 	return (
-		<CardShadow>
-			<Detail>
-				<Stats>
-					<div className="rating">
-						<h3>{game.name}</h3>
-						<p>Rating: {game.rating}</p>
-					</div>
+		<>
+			{!isLoading && (
+				<CardShadow>
+					<Detail>
+						<Stats>
+							<div className="rating">
+								<h3>{game.name}</h3>
+								<p>Rating: {game.rating}</p>
+							</div>
 
-					<Info>
-						<h3>Platforms</h3>
-						<Platforms>
-							{game.platforms.map((data) => (
-								<h3 key={data.platform.id}>{data.platform.name}</h3>
+							<Info>
+								<h3>Platforms</h3>
+								<Platforms>
+									{game.platforms.map((data) => (
+										<h3 key={data.platform.id}>{data.platform.name}</h3>
+									))}
+								</Platforms>
+							</Info>
+						</Stats>
+
+						<Media>
+							<img src={game.background_image} alt={game.background_image} />
+						</Media>
+
+						<Description>
+							<p>{game.description_raw}</p>
+						</Description>
+
+						<div className="gallery">
+							{screen.results.map((screen) => (
+								<img src={screen.image} key={screen.id} alt={screen.image} />
 							))}
-						</Platforms>
-					</Info>
-				</Stats>
-
-				<Media>
-					<img src={game.background_image} alt={game.background_image} />
-				</Media>
-
-				<Description>
-					<p>{game.description_raw}</p>
-				</Description>
-
-				<div className="gallery">
-					{screen.results.map((screen) => (
-						<img src={screen.image} key={screen.id} alt={screen.image} />
-					))}
-				</div>
-			</Detail>
-		</CardShadow>
+						</div>
+					</Detail>
+				</CardShadow>
+			)}
+		</>
 	);
 };
 
